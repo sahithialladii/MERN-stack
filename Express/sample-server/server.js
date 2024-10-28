69e678cfb1536992c013ebf67db9e7919921f026
sahithialladii <sahithialladi89@gmail.com>
2024-10-28 16:06:47 +0530

    day 7 :backend express

---
+++ Express/sample-server/server.js
@@ -0,0 +1,29 @@
+const express=require('express')
+
+//each application should have separate port to access server
+const Port=3000//can use any port but try to keep it below 8080 few are reserved should not use
+
+
+const app=express()//app will directly interact with express library
+//basic application is set
+//we need to port this to run
+
+
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.status(200).json("Hello");
+})
+
+
+
+app.listen(Port,()=>{
+    console.log("Server is runnning inn port: "+Port)
+    
+    console.log(`Server is runnning inn port: ${Port}`)
+
+
+    //custom command
+    //start command is given in package.json file
+    //npm start command can be used now to run
+})
